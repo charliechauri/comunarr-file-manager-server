51152b5ee4ea1a4ab98f1796f7b6ae994e3618e6
Carlos Echauri <carlos.echauri@gmail.com>
2017-07-10 12:33:03 -0500

    chore Create projects constants

---
+++ src/config/constants.js
@@ -0,0 +1,16 @@
+module.exports = function () {
+
+    const dbContants = {
+        host: '35.184.224.0',
+        user: 'app',
+        password: 'Comunarr2017',
+        database: 'comunarr',
+        multipleStatements: true
+    };
+
+    const constants = {
+        database: dbContants
+    };
+
+    return constants;
+}();
