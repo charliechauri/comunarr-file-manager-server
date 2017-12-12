49373cc4a8ffa54ab228203bbe391283bbb8da41
Carlos Echauri <carlos.echauri@gmail.com>
2017-12-11 23:13:11 -0600

    chore Add frontend dependency

--- app.js
+++ app.js
@@ -14,6 +14,16 @@ server.connection({
     routes: { cors: !!process.env.CORS_ENV }
 });
 
+server.route({
+    method: 'GET',
+    path: `${__dirname}/node_modules/comunarr-file-manager/dist/{file*}`,
+    handler: {
+        directory: {
+            path: '/'
+        }
+    }
+});
+
 server.register([BasicAuth, Inert], err => {
     if (err) throw err;
     server.auth.strategy('simple', 'basic', {
