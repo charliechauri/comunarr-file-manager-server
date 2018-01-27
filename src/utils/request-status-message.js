ff412d7de138135aa42000bd7d7e8cf524e65855
Ariana Hernández <arianahernandezvazquez@gmail.com>
2018-01-26 22:19:03 -0600

    chore Change exit message

--- src/utils/request-status-message.js
+++ src/utils/request-status-message.js
@@ -4,5 +4,5 @@ module.exports = {
     NOT_FOUND: Boom.notFound('No se encontraron resultados'),
     UNAUTHORIZED: Boom.unauthorized('No tienes permisos para consultar este recurso'),
     BAD_REQUEST: Boom.badRequest('No se pudo completar la acción solicitada'),
-    OK: 'La información se actualizó correctamente'
+    OK: 'La información se guardó correctamente'
 };
